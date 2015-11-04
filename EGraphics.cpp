1b3fb1e03ecf1442328adee59ecfcae3b7d319b1
FionaQi <v-fenq@microsoft.com>
2015-11-04 15:53:43 +0800

    finish EGraphics using windows APIs, fix bug

--- EGraphics.cpp
+++ EGraphics.cpp
@@ -5,7 +5,7 @@ namespace E3D{
 	EBitmap::EBitmap(const EString &filename) :name(filename), pixels(NULL),
 		pitch(0), width(0), height(0), valid(false) 
 	{
-		hBitmap = (HBITMAP)::LoadImage(GHInstance, GetPath(filename).c_str(), IMAGE_BITMAP, 0, 0, fuload);
+		hBitmap = (HBITMAP)::LoadImage(GHInstance, GetPath(filename).c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 		if (hBitmap != nullptr) {
 			bitmapHDC = ::CreateCompatibleDC(NULL);
 			::SelectObject(bitmapHDC, (HGDIOBJ)hBitmap);
@@ -52,7 +52,7 @@ namespace E3D{
 	bool EGraphics::initGraphics(HINSTANCE hinstance) {
 		GBufferedHDC = ::CreateCompatibleDC(NULL);
 
-		BITMAPINFO info = (0);
+		BITMAPINFO info = {0};
 		info.bmiHeader.biSize = sizeof(info.bmiHeader);
 		info.bmiHeader.biWidth = SCREEN_WIDTH;
 		info.bmiHeader.biHeight = SCREEN_HEIGHT;
@@ -66,6 +66,82 @@ namespace E3D{
 		::SelectObject(GBufferedHDC, GBufferHandle);
 
 		::GetObject(GBufferHandle, sizeof(DIBSECTION), &GDiBSection);
-		GDatas = (BYTE*)GDiBSection
+		GDatas = (BYTE*)GDiBSection.dsBm.bmBits;
+		GPitch = GDiBSection.dsBm.bmWidthBytes;
+
+		::SetRect(&GBufferSize, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+		GPen = ::CreatePen(PS_SOLID, 1, RGB(255, 255, 255));
+		::SelectObject(GBufferedHDC, GPen);
+
+		HFONT hfnt = (HFONT)::GetStockObject(OEM_FIXED_FONT);
+		::SelectObject(GBufferedHDC, hfnt);
+
+		::SetBkMode(GBufferedHDC, TRANSPARENT);
+
+		GZBuffer = new EFloat[SCREEN_WIDTH * SCREEN_HEIGHT];
+		memset(GZBuffer, 0, sizeof(EFloat)* SCREEN_WIDTH * SCREEN_HEIGHT);
+
+		return true;
+	}
+
+	void EGraphics::shutdownGraphics() {
+		::DeleteObject(GPen);
+		::DeleteObject(GBgBrush);
+		::DeleteObject(GBufferHandle);
+		::DeleteObject(GBufferedHDC);
+	}
+
+	void EGraphics::ClearBuffer(const EColor &c /* =EColor() */) {
+		::FillRect(GBufferedHDC, &GBufferSize, GBgBrush);
+		::memset(GZBuffer, 0, sizeof(EFloat)* SCREEN_WIDTH * SCREEN_HEIGHT);
+	}
+
+	void EGraphics::drawLine(EInt x0, EInt y0, EInt x1, EInt y1, const EColor &c) {
+		::SelectObject(GBufferedHDC, GetStockObject(DC_PEN));
+		::SetDCPenColor(GBufferedHDC, RGB(c.r, c.g, c.b));
+		::MoveToEx(GBufferedHDC, x0, y0, NULL);
+		::LineTo(GBufferedHDC, x1, y1);
+	}
+
+	void EGraphics::drawString(const EString& str, EInt x, EInt y, const EColor &c /*= EColor(255, 255, 255)*/) {
+		::SetTextColor(GBufferedHDC, RGB(c.r, c.g, c.b));
+		::TextOut(GBufferedHDC, x, y, str.c_str(), str.length());
+	}
+
+	void EGraphics::fillTriangle(EInt x0, EInt y0, EInt x1, EInt y1, EInt x2, EInt y2, const EColor &c /*= EColor(255, 255, 255)*/){
+	
+	}
+
+	void enableSmoothingMode(EBool enable) {
+	
+	}
+
+	EBool EGraphics::checkZ(EInt x, EInt y, EFloat z) {
+		EInt index = y * SCREEN_WIDTH + x;
+		EFloat divZ = 1.0f / z;
+
+		if (GZBuffer[index] > divZ)
+			return false;
+		GZBuffer[index] = divZ;
+		return true;
+	}
+	void EGraphics::setPixel(EInt x, EInt y, const EColor &c) {
+		BYTE* pSrcPix = GDatas + (GPitch * y);
+		pSrcPix += x * 4;
+		pSrcPix[0] = c.b;
+		pSrcPix[1] = c.g;
+		pSrcPix[2] = c.r;
+		pSrcPix[3] = c.a;
+	}
+
+	EColor EGraphics::getPixel(EInt x, EInt y) {
+		BYTE* pSrcPix = GDatas + (GPitch * y);
+		pSrcPix += x * 4;
+		return EColor(pSrcPix[2], pSrcPix[1], pSrcPix[0]);
+	}
+
+	void EGraphics::fillBuffer(HDC hdc) {
+		::BitBlt(hdc, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, GBufferedHDC, 0, 0, SRCCOPY);
 	}
 }
