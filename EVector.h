e1a03efd8b38c914c35ef8a3a53e1a048928087a
FionaQi <v-fenq@microsoft.com>
2015-11-06 16:57:16 +0800

    add 4D vector define

--- EVector.h
+++ EVector.h
@@ -41,7 +41,7 @@ namespace E3D {
 				lenProduct = EPSILON_E6;
 			}
 			EFloat f = dotProduct(right) / lenProduct;
-			f = Clamp(f, -1.0f, 1.0f);
+			f = Clamp<EFloat>(f, -1.0f, 1.0f);
 			return acos(f);
 		}
 
@@ -188,4 +188,99 @@ namespace E3D {
 		}
 	};
 
+	struct EVector4D {
+		static EVector4D ZERO;
+		static EVector4D UNIT_X;
+		static EVector4D UNIT_Y;
+		static EVector4D UNIT_Z;
+
+		EFloat x;
+		EFloat y;
+		EFloat z;
+		EFloat w;
+
+		EVector4D() : x(0), y(0), z(0), w(0) {}
+		EVector4D(EFloat xf, EFloat yf, EFloat zf) : x(xf), y(yf), z(zf), w(0) {}
+		EVector4D(const EVector4D& v) : x(v.x), y(v.y), z(v.z), w(v.w) {}
+
+		inline EFloat normalize() {
+			EFloat len = length();
+			if (len > EPSILON_E6) {
+				EFloat invLength = 1.0f / len;
+				x *= invLength;
+				y *= invLength;
+				z *= invLength;
+			}
+			return len;
+		}
+
+		inline EFloat length() const {
+			return sqrt(x * x + y * y + z * z);
+		}
+
+		inline EFloat dotProduct(const EVector4D& right) const {
+			return x * right.x + y * right.y + z * right.z;
+		}
+
+		inline EFloat angleBetween(const EVector4D& right) const {
+			EFloat lenProduct = length() * right.length();
+			if (lenProduct < EPSILON_E6) {
+				lenProduct = EPSILON_E6;
+			}
+			EFloat f = dotProduct(right) / lenProduct;
+			f = Clamp(f, -1.0f, 1.0f);
+			return acos(f);
+		}
+
+		inline bool isZero() const {
+			return length() < EPSILON_E6;
+		}
+
+		inline EFloat squaredLength() const {
+			return x * x + y * y + z * z;
+		}
+
+		inline EVector4D operator*(float right) const {
+			return EVector4D(x * right, y * right, z * right);
+		}
+
+		inline EVector4D operator*(const EVector4D& right) const {
+			return EVector4D(x * right.x, y * right.y, z * right.z);
+		}
+
+		inline EVector4D operator+(const EVector4D& right) const {
+			return EVector4D(x + right.x, y + right.y, z + right.z);
+		}
+
+		inline EVector4D operator-(const EVector4D& right) const {
+			return EVector4D(x - right.x, y - right.y, z - right.z);
+		}
+
+		inline EVector4D operator-(){
+			x = -x;
+			y = -y;
+			z = -z;
+
+			return *this;
+		}
+
+		inline EVector4D operator+() {
+			x = +x;
+			y = +y;
+			z = +z;
+			return *this;
+		}
+
+		inline bool operator==(const EVector4D& right) const {
+			return EqualFloat(x, right.x) && EqualFloat(y, right.y) && EqualFloat(z, right.z);
+		}
+
+		inline EVector4D operator=(const EVector4D& right) {
+			x = right.x;
+			y = right.y;
+			z = right.z;
+
+			return *this;
+		}
+	};
 }
